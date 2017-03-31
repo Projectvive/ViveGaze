5e9e17e2fb38a43ec454c918ea6fe81c180a6882
zachFokin <ztfokin@gmail.com>
2017-03-31 18:37:19 -0400

    Add files via upload

---
+++ main.js
@@ -0,0 +1,39 @@
+"use strict";
+var AutoLaunch = require('auto-launch');
+const electron = require("electron");
+const app = electron.app;  // Module to control application life.
+const BrowserWindow = electron.BrowserWindow;  // Module to create native browser window.
+
+// Keep a global reference of the window object, if you don"t, the window will
+// be closed automatically when the JavaScript object is garbage collected.
+let mainWindow = null;
+
+// Quit when all windows are closed.
+app.on("window-all-closed", function() {
+    app.quit();
+   var gazeLauncher = new AutoLaunch({
+        name: 'GazeDetector'
+    });
+     /*if(true)
+    gazeLauncher.enable();
+    else
+    	*/gazeLauncher.disable();
+});
+
+// This method will be called when Electron has finished
+// initialization and is ready to create browser windows.
+app.on("ready", function() {
+    // Create the browser
+    mainWindow = new BrowserWindow({width: 1420, height: 800});
+
+    // and load the index.html of the app.
+    mainWindow.loadURL("file://" + __dirname + "/index.html");
+
+    // Open the DevTools.
+    // mainWindow.webContents.openDevTools();
+
+    // Emitted when the window is closed.
+    mainWindow.on("closed", function() {
+        mainWindow = null;
+    });
+});
---
+++ settings.js
@@ -0,0 +1,130 @@
+"use strict";
+const EventEmitter = require("events");
+const bs = require('browser-storage');
+// Words used in program and translations
+const langEN = { //English do keep this alphabetized
+    blinkSpeed: "Blink Speed",
+    capture: "Capture",
+    closeEye: "Please hold your eye shut",
+    gaze: "Gaze",
+    guess: "Guess",
+    language: "Language",
+    lastEvent: "Last Event",
+    layout: "Layout",
+    noCamera: "No camera detected",
+    openEye: "Please hold your eye open",
+    rest: "Rest",
+    scanSpeed: "Scan Speed",
+    set: "Set",
+    settings: "Settings",
+    start: "Start",
+    stop: "Stop",
+    videofeed: "Video Feed"
+}
+//Added by MEM 3/28/2017
+const langES = { //Spanish do keep this alphabetized
+    blinkSpeed: "Velocidad de parpadeo",
+    capture: "Captura",
+    closeEye: "Por favor cierra su ojo",
+    gaze: "Mirada",
+    guess: "Adivina",
+    language: "Idioma",
+    lastEvent: "Último evento",
+    layout: "Diseño",
+    noCamera: "No se ha detectado ninguna cámara",
+    openEye: "Por favor abra su ojo",
+    rest: "Descansa",
+    scanSpeed: "Velocidad de exploración",
+    set: "Establece",
+    settings: "Ajustes",
+    start: "Comienzo",
+    stop: "Deténgase",
+    videofeed: "Video en vivo"
+}
+//Ryan Campbell
+function settings() {
+    
+	var lang = null;
+	var scanSpeed=null;
+	var gazeSpeed=null;
+	
+	
+	
+	var defaltSettings={
+		lang :langEN,
+    	scanSpeed : 2,
+    	gazeSpeed :.2,
+    		}
+    	let emitter = new EventEmitter;
+   let data=bs.getItem('settings');
+   let dataSplit= data.split(" ");
+   console.log(data);
+   console.log(dataSplit[0]);
+   console.log(dataSplit[1]);
+   console.log(dataSplit[2]);
+   switch(dataSplit[0]) {
+   		case "English":
+   			lang = langEN;
+   			break;
+   		case "Spanish":
+   			lang = langES;
+   			break;}
+   
+   scanSpeed = parseFloat(dataSplit[1],10);
+   gazeSpeed = parseFloat(dataSplit[2],10);
+    	
+   if( scanSpeed ==null)
+   	{
+   	   lang=defaltSettings.lang;
+    	   scanSpeed=defaltSettings.scanSpeed;
+   	   gazeSpeed=defaltSettings.gazeSpeed;
+   	}
+  
+    let settings = {
+        get scanSpeed() {return scanSpeed},
+        set scanSpeed(v) {scanSpeed = v;
+        let json = lang.toString() +" "+ scanSpeed+ " "+ gazeSpeed;
+        
+        console.log(json);
+		bs.setItem('settings', json);
+		emitter.emit("change");},
+
+        get gazeSpeed() {return gazeSpeed},
+        set gazeSpeed(v) {gazeSpeed = v;
+        let json = lang.toString() +" "+ scanSpeed+ " "+ gazeSpeed;
+        
+        console.log(json);
+		bs.setItem('settings', json);
+		emitter.emit("change");},
+
+        get languages() {return ["English"];},
+        get lang() {return lang;},
+        set lang(v) {switch(v) {
+            case "English":
+            	lang = "English";
+            	let json = lang +" "+ scanSpeed+ " "+ gazeSpeed;
+                console.log(json);
+          		bs.setItem('settings', json);
+                lang = langEN;
+                break;
+            case "Spanish":
+            	lang = "Spanish";
+            	json = lang +" "+ scanSpeed+ " "+ gazeSpeed;
+                console.log(json);
+          		bs.setItem('settings', json);
+                lang = langES;
+                break;
+            default:
+                throw new Error("invalid language: " + v);
+        }
+     
+		emitter.emit("change");},
+
+        addListener: (l) => emitter.addListener("change", l),
+        removeListener: (l) => emitter.removeListener("change", l)
+    };
+  
+    
+    return settings;
+}
+module.exports = settings;
